37368c5116c22ee3db4e13da503d8eefaf146865
Luis Fernando <lcruzfarfan@gmail.com>
2018-08-30 23:06:04 +0000

    Otras entidades

---
+++ app/Language.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Language extends Model
+{
+    protected $table = "languages";
+    
+    protected $fillable = ['name'];
+    
+    protected $hidden = ['created_at', 'updated_at'];
+    
+}
